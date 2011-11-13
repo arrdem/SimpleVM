7a35df786fd3fbfd63ccbb32b92767e4af6fd942
Reid McKenzie <rmckenzie92@gmail.com>
2011-11-13 14:04:25 -0600

    Math is now good to go, added generic math function.

--- src/vmath.c
+++ src/vmath.c
@@ -3,18 +3,50 @@
 
 #include "vmblock.c"
 
-void vm_add(VMBlock *a, VMBlock  *b, VMBlock  *c) {
-    *c->ptr = (*a->ptr + *b->ptr);
+VMBlock * vm_ram_malloc(VMRam*);
+
+void vm_math_generic(VMRam *ram, int a, int b, int c, char opr) {
+    int d = 0, e = 0, f = c;
+    if(ram->regs[a].used) d = *ram->regs[a].ptr;
+    if(ram->regs[b].used) e = *ram->regs[b].ptr;
+    if(!ram->regs[c].used) {
+        VMBlock * foo = vm_ram_malloc(ram);
+        f = foo->addr;
+        foo->ptr = malloc(sizeof(int));
+        printf("[MATH] WARNING - %-10i WAS NOT INITIALIZED, USING %i\n", c, f);
+    }
+    switch(opr) {
+        case '+':
+            *ram->regs[f].ptr = d + e;
+            break;
+        case '-':
+            *ram->regs[f].ptr = d - e;
+            break;
+        case '*':
+            *ram->regs[f].ptr = d * e;
+            break;
+        case '/':
+            *ram->regs[f].ptr = d / e;
+            break;
+        default:
+            printf("[MATH] ERROR - INVALID OPERATION SPECIFIED\N");
+            break;
+    }
+
 }
 
-void vm_sub(VMBlock *a, VMBlock  *b, VMBlock  *c) {
-    *c->ptr = (*a->ptr - *b->ptr);
+void vm_math_add(VMRam *ram, int a, int b, int c) {
+    vm_math_generic(ram, a, b, c, '+');
 }
 
-void vm_mult(VMBlock *a, VMBlock  *b, VMBlock  *c) {
-    *c->ptr = ((*a->ptr)*(*b->ptr));
+void vm_math_sub(VMRam *ram, int a, int b, int c) {
+    vm_math_generic(ram, a, b, c, '-');
 }
 
-void vm_div(VMBlock *a, VMBlock *b, VMBlock *c) {
-    *c->ptr = ((*a->ptr)/(*b->ptr));
+void vm_math_mult(VMRam *ram, int a, int b, int c) {
+    vm_math_generic(ram, a, b, c, '*');
+}
+
+void vm_math_div(VMRam *ram, int a, int b, int c) {
+    vm_math_generic(ram, a, b, c, '/');
 }
