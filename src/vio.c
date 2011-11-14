18fb758807f5f809bd1264ace5d07c375cbf2013
Reid McKenzie <rmckenzie92@gmail.com>
2011-11-14 10:20:48 -0600

    Added a full, pre-split read function

--- src/vio.c
+++ src/vio.c
@@ -2,12 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-    char **strings;
-    int count;
-} split;
+#include "vmtypes.c"
+#include "vmutil.c"
 
-char *vm_getline(char *line, int size)
+char *vm_io_getline(char *line, int size)
 {
     if (fgets(line, size, stdin)) {
         char *newline = strchr(line, '\n');
@@ -16,44 +14,16 @@ char *vm_getline(char *line, int size)
     return line;
 }
 
-split* vm_split(char* line, const char delim) {
-    split* s;
-    s = malloc(sizeof(split));
-    char* c = line;
-    int i, j, str_len;
-    s->count = 1;
-    str_len = 0;
+split* vm_io_read(char* prompt, int line_len) {
+    char* text;
+    text = malloc(line_len * sizeof(char));
 
-    while(*c != '\0') {
-        printf("%c   ", *c);
-        if(*c == delim) s->count++;
-        str_len++;
-        c++;
-    }
-
-    s->strings = malloc(s->count * sizeof(char*));
-
-    printf("\nMALLOC OK: %i\n", s->count);
-    while(i < s->count)
-        s->strings[i++] = malloc((str_len - s->count) * sizeof(char));
-
-    printf("RECURSIVE MALLOC OK: %i\n", (str_len - s->count));
-    c = line;
-    i = 0;
-    j = 0;
-    while(*c != '\0') {
-        printf("I:%i J:%i C:%c, %X\n", i, j, *c, *c);
-        if(*c == delim) {
-            s->strings[i++][j] = '\0';
-            j = 0;
-        } else {
-            s->strings[i][j++] = *c;
-        }
-        c++;
-    }
-    return s;
+    printf("%s", prompt);
+    vm_io_getline(text, sizeof(text));
+    return vm_util_split(text, ' ');
 }
 
+/*
 int main(void)
 {
     char text[80] = "";
@@ -72,3 +42,4 @@ int main(void)
 
     return 0;
 }
+*/
