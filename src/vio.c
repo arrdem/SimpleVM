725f02290eac3f02d8f8084806b6167290b250d7
Reid McKenzie <rmckenzie92@gmail.com>
2011-11-22 13:14:54 -0600

    Major bugfixes, added a file reading function, threw out the old line reader

--- src/vio.c
+++ src/vio.c
@@ -1,23 +1,41 @@
+/*      vio.c
+ *
+ *      Copyright 2011 Reid McKenzie <rmckenzie92@gmail.com>
+ *
+ *      This program is free software; you can redistribute it and/or
+ *      modify it under the terms of the GNU General Public License as
+ *      published by the Free Software Foundation; either version 2 of
+ *      the License, or (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public
+ *      License along with this program; if not, write to the Free
+ *      Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *      Boston, MA 02110-1301, USA.
+ *
+ *      Defines some simple IO functions used for reading code from
+ *      STDIN and files. Will also contain the printing implimentation
+ *      when I get around to writing it up.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "vmtypes.c"
 
-char *vm_io_getline(char *line, int size)
-{
-    if (fgets(line, size, stdin)) {
-        char *newline = strchr(line, '\n');
-        if (newline) *newline =  '\0';
-    }
+char* vm_io_readf(FILE* file, int line_len) {
+    char* line;
+    line = malloc(sizeof(char) * line_len);
+    fgets ( line, line_len, file );
     return line;
 }
 
-char* vm_io_read(char* prompt, int line_len) {
-    char* text;
-    text = malloc(line_len * sizeof(char));
-
+char* vm_io_read(FILE* file, char* prompt, int line_len) {
     printf("%s", prompt);
-    vm_io_getline(text, sizeof(text));
-    return text;
+    return vm_io_readf(file, line_len);
 }
