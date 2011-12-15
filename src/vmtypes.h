cba67f9e1cf48218aaede09d9cff0a61462067e2
Reid McKenzie <rmckenzie92@gmail.com>
2011-12-14 22:05:17 -0600

    Some changes to the type system.. renamed VMInstr to VMLine and made some edits

--- src/vmtypes.h
+++ src/vmtypes.h
@@ -2,27 +2,26 @@
 #define _VMBLOCK_C_
 
 typedef struct {
-    int *ptr;
-    int type;
-    int addr;
-    int used;
+    int*     ptr;
+    int      type;
+    int      addr;
+    int      used;
 } VMBlock;
 
 typedef struct {
     VMBlock* regs;
-    int size;
-    int used;
+    int      size;
+    int      used;
 } VMRam;
 
 typedef struct {
-    char* opcode;
-    int  args[6];
-    // basically just overkill the ram..
-} VMInstr;
+    char*    text;
+    int*     code;
+} VMLine;
 
 typedef struct {
     VMRam*   memory;
-    VMInstr* code;
+    VMLine*  code;
     int      lines;
     int      cursor;
     char*    errmsg;
