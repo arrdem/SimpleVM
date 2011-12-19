c6b9bb16f3b4cda9dcb5676012d20b99aaa2e9b4
Reid McKenzie <rmckenzie92@gmail.com>
2011-12-19 01:12:43 -0500

    added a LinkedList implimentation for a circular queue

---
+++ src/linklist.c
@@ -0,0 +1,41 @@
+#include "linklist.h"
+
+#ifndef _LINKLIST_C_
+#define _LINKLIST_C_
+
+void ll_insert(ll* c, void* d) {
+    ll* n = malloc(sizeof(ll));
+    n->next = c->next;
+    n->data = d;
+    c->next = n;
+}
+
+int ll_search(ll* o, void* d) {
+    int i = 0;
+    ll* c;
+    c = o;
+    while((c != o) && (c->data != d) && (c->next)) {
+        c = c->next;
+        i++;
+    }
+    if(c->data == d) return i;
+    else return -1;
+}
+
+int ll_delete(ll* o, void* d) {
+    ll *c, *t;
+    t = o;
+    c = t->next;
+    while((c != o) && (c->next) && (c->data != d)) {
+        t = c;
+        c = c->next;
+    }
+    if(c->data == d) {
+        t->next = c->next;
+        free(c);
+        return 1;
+    }
+    return -1;
+}
+
+#endif
---
+++ src/linklist.h
@@ -0,0 +1,13 @@
+#ifndef _LINKLIST_H_
+#define _LINKLIST_H_
+
+typedef struct {
+    void* next;
+    void* data;
+} ll;
+
+void ll_insert(ll*, void*);
+int ll_search(ll*, void*);
+int ll_delete(ll*, void*);
+
+#endif // _LINKLIST_H_
